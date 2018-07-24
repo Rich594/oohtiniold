2a1d24f1fc8644c1276ab40eb4686f2658dae96b
Richard Anderson <37628732+Rich594@users.noreply.github.com>
2018-07-24 16:32:28 +0100

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,8 +1,8 @@
 <?php // Example 26-1: functions.php
   $dbhost  = 'localhost';    // Unlikely to require changing
   $dbname  = 'robinsnest';   // Modify these...
-  $dbuser  = 'rich';   // ...variables according
-  $dbpass  = '123';   // ...to your installation
+  $dbuser  = '';   // ...variables according
+  $dbpass  = '';   // ...to your installation
   $appname = "Oohtini"; // ...and preference
 
   $link = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
